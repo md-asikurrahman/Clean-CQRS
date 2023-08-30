140cbd98e3eb0c0823fc2c81aa58e4754dba0731
nillakash4097@gmail.com <nillakash4097@gmail.com>
2023-08-30 11:20:18 +0600

    commitInit

---
+++ src/WebAPI/Controllers/V2/PostsController.cs
@@ -0,0 +1,21 @@
+﻿using APIwithCQRS.Dmain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers.V2
+{
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class PostsController : Controller
+    {
+        [HttpGet]
+        [Route("{id}")]
+
+        public async Task<IActionResult> GetById(int id)
+        {
+            var post = new Post { Id = id, Text = "Assalamu-alaikum vai" };
+            return Ok(post);
+        }
+
+    }
+}
